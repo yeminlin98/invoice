3e46cc22fa5784fb70ab1bc7ca163a2a85e6bb49
yeminlin98 <yeminlin062@gmail.com>
2023-11-23 00:55:24 +0630

    ready for pj

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "index.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+}
+
