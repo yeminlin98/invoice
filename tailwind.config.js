f366a18c5eefb854e479527ea18080719984549a
yeminlin98 <yeminlin062@gmail.com>
2023-11-24 02:48:17 +0630

    invoice

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
+    fontFamily:{
+     sans :['Montserrat','Padauk'," sans-serif"]
+
+    },
     extend: {},
   },
   plugins: [
